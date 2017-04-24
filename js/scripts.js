6e8bccfeaf7cbaad2c6732e0ce018a3147f40e32
Nick <usaspiff@yahoo.com>
2017-04-23 21:51:25 -0700

    Code submission to fcc
    Final JS code for fcc submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,85 @@
 $(document).ready(function() {
-  var timePause = 5;
-  var timeWork = 25;
-  
- //Default starting Pause time 
-   $("#pauseDisplay").html(timePause + " minutes");
- 
- //Adjusting Pause time with + and - buttons
-  $("p").on("click", function(pauseTime) {
-    if (this.id === "pausePlus") {
-      timePause ++;
-      $("#pauseDisplay").html(timePause + " minutes");
-    } else if (this.id === "pauseMinus" && timePause >2) {
-      timePause --;
-      $("#pauseDisplay").html(timePause + " minutes");
-    }  else if (this.id === "pauseMinus" && timePause <=0) {
-      console.log("Time can't be negatif!");
-    } else if (this.id === "pauseMinus" && timePause <=2) {
-      timePause --;
-      $("#pauseDisplay").html(timePause + " minute");
-    }
-    console.log("Pause time is " + timePause + " minute(s)");
-  });
- 
-  //Clock countdown
-  var displaySec;
- 
-    function changeColor() {
-      displaySec = setInterval(countDownSec, 1000);
-    }
- //*** Work left off here
-    function countDownSec() {
-      var oElem = document.getElementById('mainDisplaySec');
-      oElem.style.color = oElem.style.color == 'lime' ? 'blue' : 'lime';
-    }
- //to Pause countdown
-    function stopCountdown() {
-      clearInterval(displaySec);
-    }
-  //Run timer GO button action
-  $("#goClock").on("click", function(changeColor));
-  //pause button action
-   $("#pauseClock").on("click", function(stopCountdown));
-/*
- $("#pauseClock").on("click", function(stopCountdown));
- */
+    var timePause = 2;
+    var timeWork = 2;
+
+    //Default starting status
+    $("#pauseDisplay").html(timePause);
+    $("#workDisplay").html(timeWork);
+
+    //Adjusting Pause time with + and - buttons
+
+    $(".btn-defaultP").on("click", function(pauseTime) {
+        if (this.id === "pausePlus") {
+            timePause++;
+            $("#pauseDisplay").html(timePause);
+        } else if (this.id === "pauseMinus" && timePause > 0) {
+            timePause--;
+            $("#pauseDisplay").html(timePause);
+        } else if (this.id === "pauseMinus" && timePause <= 0) {
+            console.log("Can't travel back in time yet!");
+        }
+        console.log("Pause time is " + timePause + " minute(s)");
+    });
+
+    //Adjusting Work time with + and - buttons
+    $(".btn-defaultW").on("click", function(workTime) {
+        if (this.id === "workPlus") {
+            timeWork++;
+            $("#workDisplay").html(timeWork);
+        } else if (this.id === "workMinus" && timeWork > 0) {
+            timeWork--;
+            $("#workDisplay").html(timeWork);
+        } else if (this.id === "workMinus" && timeWork <= 0) {
+            console.log("Can't travel back in time yet!");
+        }
+        console.log("Work time is " + timeWork + " minute(s)");
+    });
+
+    //Clock countdown function
+    $("#goClock").click(function() {
+        var counter = setInterval(workTimer, 1000);
+        var count = timeWork * 60;
+        var pauseCount = timePause * 60;
+        //console.log(count);
+
+        function workTimer() {
+            //changes buttons status and display
+            $("#settingsP, #settingsW, #goClock").hide();
+            $("#mainHeader").html("Work time left:");
+            count -= 1;
+            if (count === 0) {
+                //add buzzer
+                clearInterval(counter);
+
+                var pauseCounter = setInterval(pauseTimer, 1000);
+            }
+
+            if (count % 60 >= 10) {
+                $("#mainDisplayTime").html(Math.floor(count / 60) + ":" + count % 60);
+            } else {
+                $("#mainDisplayTime").html(Math.floor(count / 60) + ":" + "0" + count % 60);
+            }
+
+            function pauseTimer() {
+                //console.log(count);
+                $("#mainHeader").html("Pause time left:");
+
+                pauseCount -= 1;
+                if (pauseCount === 0) {
+                    //add buzzer
+                    clearInterval(pauseCounter);
+                    $("#settingsP, #settingsW, #goClock").show();
+                    $("#mainHeader").html("Clock");
+                }
+
+                if (pauseCount % 60 >= 10) {
+                    $("#mainDisplayTime").html(Math.floor(pauseCount / 60) + ":" + pauseCount % 60);
+                } else {
+                    $("#mainDisplayTime").html(Math.floor(pauseCount / 60) + ":" + "0" + pauseCount % 60);
+                }
+
+            }
+        }
+    });
+
 });
